717d762dd39d340923a29937810486dc0e4969ba
Ibai Gallego <ibimasteryt@gmail.com>
2025-05-28 16:40:38 +0200

    Remove chatbot feature from start page template and clean up related scripts

--- app/config.py
+++ app/config.py
@@ -3,4 +3,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'mysql://test:test@127.0.0.1/gravitytest'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SHERLOCK_PATH = r"G:\Dam2\testSherlock\sherlock-master\sherlock_project\sherlock.py"
+    
+    # CONFIGURAION DE LA API DE OPENAI
     OPENAI_API_KEY = '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'
